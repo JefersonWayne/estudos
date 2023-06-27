f13966aeb82ddb853e9b9e9dd1c2d6ef082dcd37
Flávio Jeferson <133133831+flaviojeferson@users.noreply.github.com>
2023-06-27 11:26:47 -0300

    Update script.js

--- javascript/hora-do-dia/scripts/script.js
+++ javascript/hora-do-dia/scripts/script.js
@@ -4,24 +4,21 @@ function update() {
     var saudacao = document.getElementById("saudacao");
     var hora = document.getElementById("hora-atual");
     var imagem= document.getElementById("image");
-
-    hora_atual = 1;
-
     if (hora_atual >= 0 && hora_atual < 6) {
-        hora.textContent = `O relógio diz que são ${hora_atual} hora(s) da madrugada!`;
-        saudacao.textContent = "Boa madrugada! Bem-vindo ao novo dia!";
+        hora.innerHTML = "O relógio diz que são <strong>" + hora_atual + " horas</strong> da madrugada!";
+        saudacao.innerHTML = "<strong>Boa madrugada!</strong> Bem-vindo ao novo dia!";
         imagem.src = "images/madrugada.jpg";
     } else if (hora_atual >= 6 && hora_atual < 12) {
-        hora.textContent = `O relógio diz que são ${hora_atual} horas da manhã!`;
-        saudacao.textContent = "Bom dia! Espero que seu dia seja maravilhoso!";
+        hora.innerHTML = "O relógio diz que são <strong>" + hora_atual + " horas</strong> da manhã!";
+        saudacao.innerHTML = "<strong>Bom dia!</strong> Espero que seu dia seja maravilhoso!";
         imagem.src = "images/manha.jpg";
     } else if (hora_atual >= 12 && hora_atual < 18) {
-        hora.textContent = `O relógio diz que são ${hora_atual} horas da tarde!`;
-        saudacao.textContent = "Boa tarde! Tenha uma tarde produtiva e agradável!";
+        hora.innerHTML = "O relógio diz que são <strong>" + hora_atual + " horas</strong> da tarde!";
+        saudacao.innerHTML = "<strong>Boa tarde!</strong> Tenha uma tarde produtiva e agradável!";
         imagem.src = "images/tarde.jpg";
     } else if (hora_atual >= 18 && hora_atual < 24) {
-        hora.textContent = `O relógio diz que são ${hora_atual} horas da noite!`;
-        saudacao.textContent = "Boa noite! Desejo que sua noite seja tranquila e revigorante!";
+        hora.innerHTML = "O relógio diz que são <strong>" + hora_atual + " horas</strong> da noite!";
+        saudacao.innerHTML = "<strong>Boa noite!</strong> Desejo que sua noite seja tranquila e revigorante!";
         imagem.src = "images/noite.jpg";
     }
 }
