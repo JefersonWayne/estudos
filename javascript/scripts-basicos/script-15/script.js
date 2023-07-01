8d2fd96ff1333fa114d1dff43c3a1bd1914bd2da
Flávio Jeferson <133133831+flaviojeferson@users.noreply.github.com>
2023-06-30 21:11:30 -0300

    Estudos de repetições em JS

---
+++ javascript/scripts-basicos/script-15/script.js
@@ -0,0 +1,43 @@
+
+/*
+    var i = 1;
+    var i_max = 300;
+
+    while (i <= i_max) {
+        console.log(`Esta é a mensagem -> ${i}°!`);
+        i++; i = i + 1; 
+    }
+*/
+
+/*
+    var i = 1;
+    var i_max = 10;
+
+    do {
+        console.log(`Esta é a mensagem -> ${i}°!`);
+        i++; i = i + 1;
+    } while (i <= i_max);
+*/
+
+/*
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+    console.log("Olá, estou testando repetições em JavaScript");
+*/
---
+++ javascript/scripts-basicos/script-16/script.js
@@ -0,0 +1,14 @@
+
+console.log('Vai começar o exemplo!')
+console.log('')
+for(var i = 1; i <=3; i++) {
+    if (i < 3) {
+        console.log(`i ainda é menor que 3`);
+        console.log('');
+    } else if (i >= 3) {
+        console.log(`i é igual a 3`);
+        console.log('');
+    }
+}
+console.log('i é maior que 3')
+console.log('')
